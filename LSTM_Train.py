64703bb813ca356e833fc94248c0dd6106a9239e
Dylan Yung <YungDexiong@hotmail.com>
2020-03-03 17:22:26 -0800

    fixed name and print every 500

--- LSTM_Train.py
+++ LSTM_Train.py
@@ -21,7 +21,7 @@ from Utilities.Train_Util import plot_losses, timeSince
 
 # Optional command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--name', help='Name of the Session', nargs='?', default='Last', type=str)
+parser.add_argument('--name', help='Name of the Session', nargs='?', default='first_lstm', type=str)
 parser.add_argument('--hidden_size', help='Size of the hidden layer of LSTM', nargs='?', default=256, type=int)
 parser.add_argument('--lr', help='Learning rate', nargs='?', default=0.005, type=float)
 parser.add_argument('--num_epochs', help='Number of epochs', nargs='?', default=1000, type=int)
