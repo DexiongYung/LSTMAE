c47e500f3bdc0ff93c6c71d03b4dd18069ee005c
Dylan Yung <YungDexiong@hotmail.com>
2020-03-03 22:02:30 -0800

    fixed DS

--- DataSetUtils/NameDS.py
+++ DataSetUtils/NameDS.py
@@ -3,9 +3,9 @@ from torch.utils.data import Dataset
 
 
 class NameDataset(Dataset):
-    def __init__(self, df: DataFrame, col_name: str):
-        df = df[col_name].str.lower().dropna()
-        self.data_frame = df[df['count'] > 5000]
+    def __init__(self, df: DataFrame, col_name: str):        
+        df = df[df['count'] > 5000]
+        self.data_frame = df[col_name].str.lower().dropna()
 
     def __len__(self):
         return len(self.data_frame)
