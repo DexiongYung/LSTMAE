103814110c7b09df9ecf9e8d8cd083d9823b4c15
Dylan Yung <YungDexiong@hotmail.com>
2020-03-13 15:02:30 -0700

    rearranged code

--- LSTM_Train_Batch.py
+++ LSTM_Train_Batch.py
@@ -71,8 +71,9 @@ def train(x: str):
     src_x = list(map(lambda s: SOS + s + (PAD * ((max_len - len(s)) - 1)), x))
     trg_x = list(map(lambda s: s + EOS + (PAD * ((max_len - len(s)) - 1)), x))
 
-    trg = targetsTensor(trg_x, max_len, OUT_CHARS).to(DEVICE)
     src = indexTensor(src_x, max_len, IN_CHARS).to(DEVICE)
+    trg = targetsTensor(trg_x, max_len, OUT_CHARS).to(DEVICE)
+    
     lstm_hidden = lstm.initHidden(batch_sz)
     lstm_hidden = (lstm_hidden[0].to(DEVICE), lstm_hidden[1].to(DEVICE))
 
