9793eb1c36ad7d9da2f1e8180df01c8914a70f5c
Dylan Yung <YungDexiong@hotmail.com>
2020-03-13 14:29:26 -0700

    Added probs generating script

---
+++ Script.py
@@ -0,0 +1,23 @@
+from pandas import DataFrame
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file_pth', help='Path of file', nargs='?', type=str)
+parser.add_argument('--save_pth', help='Path of the save csv', nargs='?', type=str)
+
+args = parser.parse_args()
+df = pd.read_csv(args.file_pth)
+
+sum = df['count'].sum()
+
+probs = []
+
+for idx, row in df.iterrows():
+    count = row['count']
+    probability = count / sum
+    probs.append(probability)
+
+df['probs'] = probs
+
+df.to_csv(args.save_pth)
