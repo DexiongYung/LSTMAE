b45228ef9ec3314c00eab263c32abb5ba5b039ff
Dylan Yung <YungDexiong@hotmail.com>
2020-03-14 01:51:00 -0700

    batch to 3k

--- LSTM_Train_Batch.py
+++ LSTM_Train_Batch.py
@@ -31,9 +31,9 @@ parser.add_argument('--num_layers', help='Number of layers', nargs='?', default=
 parser.add_argument('--train_file', help='File to train on', nargs='?', default='Data/FirstNames.csv', type=str)
 parser.add_argument('--column', help='Column header of data', nargs='?', default='name', type=str)
 parser.add_argument('--print', help='Print every', nargs='?', default=100, type=int)
-parser.add_argument('--batch', help='Batch size', nargs='?', default=250, type=int)
+parser.add_argument('--batch', help='Batch size', nargs='?', default=3000, type=int)
 parser.add_argument('--continue_training', help='Boolean whether to continue training an existing model', nargs='?',
-                    default=True, type=bool)
+                    default=False, type=bool)
 
 # Parse optional args from command line and save the configurations into a JSON file
 args = parser.parse_args()
